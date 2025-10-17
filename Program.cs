741b196c265e0ad1d32d2b6738400e7f04f46591
joaopires64 <joaogab930@gmail.com>
2025-10-17 11:46:44 -0300

    entrega desafio API

--- Program.cs
+++ Program.cs
@@ -5,9 +5,11 @@ using TrilhaApiDesafio.Context;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+var connectionString = builder.Configuration.GetConnectionString("ConexaoPadrao");
 
+builder.Services.AddDbContext<OrganizadorContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
 builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 
